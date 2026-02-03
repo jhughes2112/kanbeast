e1ef490c7ab4be747dce71ce8561c0262552c78b
Jason Hughes <jhughes@reachablegames.com>
2026-02-02 20:32:27 -0600

    Cleanup network on re-run.

--- run-docker.bat
+++ run-docker.bat
@@ -5,12 +5,12 @@ set IMAGE_NAME=kanbeast
 set CONTAINER_NAME=kanbeast-server
 set NETWORK_NAME=kanbeast-network
 
-docker network create %NETWORK_NAME%
-
 docker build -t %IMAGE_NAME% -f Dockerfile .
 if errorlevel 1 exit /b 1
 
 docker rm -f %CONTAINER_NAME% >nul 2>&1
+docker network rm kanbeast-network >nul 2>&1
+docker network create %NETWORK_NAME%
 
 start http://localhost:8080
 
