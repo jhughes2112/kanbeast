8a73f704c3dd755ba87a65242184fd42e024c7f4
Jason Hughes <jhughes@reachablegames.com>
2026-02-02 20:12:56 -0600

    Fixed up the docker run batch file a bit.

--- run-docker.bat
+++ run-docker.bat
@@ -4,12 +4,8 @@ setlocal
 set IMAGE_NAME=kanbeast
 set CONTAINER_NAME=kanbeast-server
 set NETWORK_NAME=kanbeast-network
-set VOLUME_NAME=kanbeast-data
 
 docker network inspect %NETWORK_NAME% >nul 2>&1 || docker network create %NETWORK_NAME% >nul
-if errorlevel 1 exit /b 1
-
-docker volume inspect %VOLUME_NAME% >nul 2>&1 || docker volume create %VOLUME_NAME% >nul
 if errorlevel 1 exit /b 1
 
 docker build -t %IMAGE_NAME% -f Dockerfile .
@@ -17,13 +13,13 @@ if errorlevel 1 exit /b 1
 
 docker rm -f %CONTAINER_NAME% >nul 2>&1
 
-docker run -d ^
+docker run --rm -it ^
   --name %CONTAINER_NAME% ^
   --network %NETWORK_NAME% ^
   -p 8080:8080 ^
   -e ASPNETCORE_ENVIRONMENT=Production ^
   -e ASPNETCORE_URLS=http://+:8080 ^
-  -v %VOLUME_NAME%:/app/data ^
+  -v "%cd%"/env:/app/env ^
   %IMAGE_NAME%
 if errorlevel 1 exit /b 1
 
